0ddc987bd46dc886266cd54e386eb18657304d91
namk <namk6130@pointpark.com>
2024-03-11 11:24:37 +0900

    공지사항

--- src/pages/notice/detail/index.js
+++ src/pages/notice/detail/index.js
@@ -1,7 +1,24 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import ContentsLayout from "../../../components/layout";
 
 const NoticeDetailPage = () => {
-  return <div>공지사항 상세페이지</div>;
+  const param = useParams();
+
+  return (
+    <ContentsLayout>
+      <h2 className="noticeDetail__title">
+        공지사항 상세페이지테스트 {param.id}
+      </h2>
+
+      <p className="noticeDetail__contents">
+        공지사항 내용입니다.공지사항 내용입니다.공지사항 내용입니다.공지사항
+        내용입니다.공지사항 내용입니다.공지사항 내용입니다.공지사항
+        내용입니다.공지사항 내용입니다.공지사항 내용입니다.공지사항
+        내용입니다.공지사항 내용입니다.
+      </p>
+    </ContentsLayout>
+  );
 };
 
 export default NoticeDetailPage;
--- src/pages/notice/index.js
+++ src/pages/notice/index.js
@@ -1,11 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ContentsLayout from "../../components/layout";
 
 const NoticeMainPage = () => {
   return (
-    <div>
-      <Link to={`/notice/detail/${1}`}>공지사항 리스트 1</Link>
-    </div>
+    <ContentsLayout>
+      <div className="noticeMain__listItem">
+        <Link to={`/notice/detail/${1}`}>
+          <span>공지사항 리스트 1</span>
+          <span>20xx.xx.xx</span>
+        </Link>
+      </div>
+
+      <div className="noticeMain__listItem">
+        <Link to={`/notice/detail/${2}`}>
+          <span>공지사항 리스트 2</span>
+          <span>20xx.xx.xx</span>
+        </Link>
+      </div>
+
+      <div className="noticeMain__listItem">
+        <Link to={`/notice/detail/${3}`}>
+          <span>공지사항 리스트 3</span>
+          <span>20xx.xx.xx</span>
+        </Link>
+      </div>
+    </ContentsLayout>
   );
 };
 
