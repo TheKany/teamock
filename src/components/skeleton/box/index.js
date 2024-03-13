2b0e3e2097c63c7427b45b4f2572569cd09f58b7
namk <namk6130@pointpark.com>
2024-03-13 11:17:30 +0900

    정보센터 로딩 추가

---
+++ src/components/skeleton/box/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SkeletonBox = () => {
+  return (
+    <div className="skeleton__Box">
+      <span>정</span>
+      <span>보</span>
+      <span>찾</span>
+      <span>는</span>
+      <span>중</span>
+    </div>
+  );
+};
+
+export default SkeletonBox;
--- src/pages/info/index.js
+++ src/pages/info/index.js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from "react";
 import MvpAward from "../../components/svg/mvpAward";
 import WinAward from "../../components/svg/winAward";
+import SkeletonBox from "../../components/skeleton/box";
 
 const InfoCentreMainPage = () => {
   // 임시 선수 데이터
   const [data, setData] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const response = await fetch("/data/player/index.json");
-        const jsonData = await response.json();
-
-        setData(jsonData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
+      // try {
+      //   const response = await fetch("/data/player/index.json");
+      //   const jsonData = await response.json();
+      //   setData(jsonData);
+      // } catch (error) {
+      //   console.error("Error fetching data:", error);
+      // }
     };
 
     fetchData();
@@ -56,114 +56,128 @@ const InfoCentreMainPage = () => {
   }, [openId]);
 
   return (
-    <main className="info-container">
-      <div className="info__teamName-container">
-        <h2 className="info__teamName">AM SIXERS</h2>
-        <p className="info__startDate">SINCE 2023.10.03</p>
-      </div>
+    <>
+      {!data ? (
+        <SkeletonBox />
+      ) : (
+        <main className="info-container">
+          <div className="info__teamName-container">
+            <h2 className="info__teamName">AM SIXERS</h2>
+            <p className="info__startDate">SINCE 2023.10.03</p>
+          </div>
 
-      <div className="info__teamList-container">
-        {/* 테이블 헤더 (변경 x) */}
-        <div className="info__teamList-item th">
-          <div className="info__item photo th"></div>
+          <div className="info__teamList-container">
+            {/* 테이블 헤더 (변경 x) */}
+            <div className="info__teamList-item th">
+              <div className="info__item photo th"></div>
 
-          {/* 기본 정보 */}
-          <p className="info__item subInfo th"></p>
+              {/* 기본 정보 */}
+              <p className="info__item subInfo th"></p>
 
-          {/* 별명 */}
-          <p className="info__item nickname">별명</p>
+              {/* 별명 */}
+              <p className="info__item nickname">별명</p>
 
-          {/* 승률 */}
-          <p className="info__item winRate">승률</p>
+              {/* 승률 */}
+              <p className="info__item winRate">승률</p>
 
-          {/* 출석율 */}
-          <p className="info__item AttRate">출석율</p>
+              {/* 출석율 */}
+              <p className="info__item AttRate">출석율</p>
 
-          {/* 플레이스타일 */}
-          <p className="info__item style">플레이 스타일</p>
-        </div>
-
-        {data?.map((el, idx) => {
-          return (
-            // 선수
-            <div
-              key={`No.${idx + 1}: ${el.id}_${el.name}`}
-              className={`info__teamList-item ${
-                openId === el.id ? "open" : ""
-              }`}
-            >
-              <button
-                className="info__item-btn"
-                onClick={() => onClickProfile(el.id)}
-              >
-                <div className="info__item photo"></div>
-
-                {/* 기본 정보 */}
-                <div className="info__item subInfo">
-                  <p className="info__subInfo-text number">#{el.id}</p>
-                  <p className="info__subInfo-text position">{el.position}</p>
-                  <p className="info__subInfo-text name">{el.name}</p>
-                </div>
-
-                {/* 별명 */}
-                <p className="info__item nickname">{el.nickname}</p>
-
-                {/* 승률 */}
-                <p className="info__item winRate">{el.winRate}%</p>
-
-                {/* 출석율 */}
-                <p className="info__item attRate">{el.attRate}%</p>
-
-                {/* 플레이스타일 */}
-                <p className="info__item style">{el.style}</p>
-              </button>
-
-              {openId === el.id ? (
-                <div className="info__item-detail">
-                  <div className="info__detail-img">
-                    <img src={`/img/${el.imgSrc}`} alt="선수 사진" />
-                  </div>
-                  <div className="info__detail-contents">
-                    <div className="info__contents basic">
-                      <span className="info__basic name">{el.name}</span>
-                      <span className="info__basic nickname">
-                        ({el.nickname})
-                      </span>
-                      <span className="info__basic age">{el.age}</span>
-                    </div>
-                    <div className="info__contents health">
-                      <span className="info__health height">180 cm</span>
-                      <span className="info__health weight">80 kg</span>
-                    </div>
-                    <div className="info__contents style">{el.style}</div>
-                    <div className="info__contents mvp">
-                      <p className="info__mvp-title">MVP</p>
-                      <div className="info__mvp-awards">
-                        {Array.from({ length: el.winAwards }, (_, index) => (
-                          <div key={index} className="mvpAward">
-                            <MvpAward />
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                    <div className="info__contents win">
-                      <p className="info__win-title">WIN</p>
-                      <div className="info__win-awards">
-                        {Array.from({ length: el.mvpAwards }, (_, index) => (
-                          <div key={index} className="winAward">
-                            <WinAward />
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              ) : null}
+              {/* 플레이스타일 */}
+              <p className="info__item style">플레이 스타일</p>
             </div>
-          );
-        })}
-      </div>
-    </main>
+
+            {data?.map((el, idx) => {
+              return (
+                // 선수
+                <div
+                  key={`No.${idx + 1}: ${el.id}_${el.name}`}
+                  className={`info__teamList-item ${
+                    openId === el.id ? "open" : ""
+                  }`}
+                >
+                  <button
+                    className="info__item-btn"
+                    onClick={() => onClickProfile(el.id)}
+                  >
+                    <div className="info__item photo"></div>
+
+                    {/* 기본 정보 */}
+                    <div className="info__item subInfo">
+                      <p className="info__subInfo-text number">#{el.id}</p>
+                      <p className="info__subInfo-text position">
+                        {el.position}
+                      </p>
+                      <p className="info__subInfo-text name">{el.name}</p>
+                    </div>
+
+                    {/* 별명 */}
+                    <p className="info__item nickname">{el.nickname}</p>
+
+                    {/* 승률 */}
+                    <p className="info__item winRate">{el.winRate}%</p>
+
+                    {/* 출석율 */}
+                    <p className="info__item attRate">{el.attRate}%</p>
+
+                    {/* 플레이스타일 */}
+                    <p className="info__item style">{el.style}</p>
+                  </button>
+
+                  {openId === el.id ? (
+                    <div className="info__item-detail">
+                      <div className="info__detail-img">
+                        <img src={`/img/${el.imgSrc}`} alt="선수 사진" />
+                      </div>
+                      <div className="info__detail-contents">
+                        <div className="info__contents basic">
+                          <span className="info__basic name">{el.name}</span>
+                          <span className="info__basic nickname">
+                            ({el.nickname})
+                          </span>
+                          <span className="info__basic age">{el.age}</span>
+                        </div>
+                        <div className="info__contents health">
+                          <span className="info__health height">180 cm</span>
+                          <span className="info__health weight">80 kg</span>
+                        </div>
+                        <div className="info__contents style">{el.style}</div>
+                        <div className="info__contents mvp">
+                          <p className="info__mvp-title">MVP</p>
+                          <div className="info__mvp-awards">
+                            {Array.from(
+                              { length: el.winAwards },
+                              (_, index) => (
+                                <div key={index} className="mvpAward">
+                                  <MvpAward />
+                                </div>
+                              )
+                            )}
+                          </div>
+                        </div>
+                        <div className="info__contents win">
+                          <p className="info__win-title">WIN</p>
+                          <div className="info__win-awards">
+                            {Array.from(
+                              { length: el.mvpAwards },
+                              (_, index) => (
+                                <div key={index} className="winAward">
+                                  <WinAward />
+                                </div>
+                              )
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  ) : null}
+                </div>
+              );
+            })}
+          </div>
+        </main>
+      )}
+    </>
   );
 };
 
